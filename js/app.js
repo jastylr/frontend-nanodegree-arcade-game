9ec7a4ff8a0125d04d710db7e8dccd2056fe6b0f
jastylr <jtaylor@faultlinemedia.com>
2015-05-04 09:22:44 -0700

    Add inital functionality to game

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
+var engine = this.Engine = this.Engine || {};
+
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(posX, posY) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    this.x = posX;
+    this.y = posY;
+    this.width = 101;
+    this.height = 171;
+    this.speed = Math.floor(Math.random() * (300 - 100 + 1) + 100);
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -11,9 +18,26 @@ var Enemy = function() {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
+    
+    // Clear the previous enemy image
+    ctx.clearRect(this.x, this.y, this.width, this.height);
+
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.speed * dt;
+    // Once the enemy reaches the far right bounds, reset it's x position
+    if (this.x > 505) {
+        this.x = -101;
+    }
+}
+
+Enemy.prototype.setRandomSpeed = function(minSpeed, maxSpeed) {
+    this.speed = Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+Enemy.prototype.checkCollisions = function() {
+
 }
 
 // Draw the enemy on the screen, required method for game
@@ -24,12 +48,68 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
+    this.width = 101;
+    this.height = 171;
+    this.speed = 10;
+    this.stageComplete = false;
+}
+
+Player.prototype.update = function(dt) {
+    if (this.y <= 0) {
+        this.y = 0;
+        this.stageComplete = true;
+    }
+
+    if (this.y >= 606)
+        engine.endGame();
+}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+Player.prototype.reset = function() {
+    this.x = (505 / 2) - (this.width / 2);
+    this.y = 606 - this.height - 28;
+    this.stageComplete = false;
+}
+
+Player.prototype.handleInput = function(key) {
+
+    var distanceX = this.width;
+    var distanceY = this.height / 2; 
+    
+    // See which key was pressed
+    if (key === 'left') {
+        this.x -= distanceX;
+    }
+    else if (key === 'right') {
+        this.x += distanceX;
+    }
+    else if (key === 'up') {
+        this.y -= distanceY;
+    }
+    else if (key === 'down') {
+        this.y += distanceY;
+    }
+
+}
 
 
 // Now instantiate your objects.
+
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies = [];
+for (var i=0; i < 3; i++) {
+    allEnemies.push(new Enemy(50, (i*84) + 62));
+}
 
+// Create a Player and reset it to defaults
+var player = new Player();
+player.reset();
 
 
 // This listens for key presses and sends the keys to your
@@ -44,3 +124,4 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
--- js/engine.js
+++ js/engine.js
@@ -15,6 +15,7 @@
  */
 
 var Engine = (function(global) {
+
     /* Predefine the variables we'll be using within this scope,
      * create the canvas element, grab the 2D context for that canvas
      * set the canvas elements height/width and add it to the DOM.
@@ -23,7 +24,8 @@ var Engine = (function(global) {
         win = global.window,
         canvas = doc.createElement('canvas'),
         ctx = canvas.getContext('2d'),
-        lastTime;
+        lastTime,
+        gameOver = false;
 
     canvas.width = 505;
     canvas.height = 606;
@@ -64,6 +66,15 @@ var Engine = (function(global) {
      * game loop.
      */
     function init() {
+         // Setup an event listeners on the Start and Play Again buttons
+        document.getElementById('start-btn').addEventListener('click', function() {
+            startGame();
+        });
+
+        document.getElementById('play-again').addEventListener('click', function() {
+            reset();
+        });
+
         reset();
         lastTime = Date.now();
         main();
@@ -94,7 +105,16 @@ var Engine = (function(global) {
         allEnemies.forEach(function(enemy) {
             enemy.update(dt);
         });
-        player.update();
+
+        // See if the player has reached the end of the stage
+        // and if not, then update the player otherwise call
+        // the endGame function to end the game
+        if (!player.stageComplete) {
+            player.update();
+        }
+        else {
+            endGame();
+        }
     }
 
     /* This function initially draws the "game level", it will then call
@@ -148,11 +168,13 @@ var Engine = (function(global) {
         /* Loop through all of the objects within the allEnemies array and call
          * the render function you have defined.
          */
-        allEnemies.forEach(function(enemy) {
-            enemy.render();
-        });
+        if (!gameOver) {
+            allEnemies.forEach(function(enemy) {
+                enemy.render();
+            });
 
-        player.render();
+            player.render();
+        }
     }
 
     /* This function does nothing but it could have been a good place to
@@ -160,7 +182,31 @@ var Engine = (function(global) {
      * those sorts of things. It's only called once by the init() method.
      */
     function reset() {
-        // noop
+        gameOver = false;
+        score = 0
+        document.getElementById('game-over').style.display = 'none';
+        document.getElementById('game-over-overlay').style.display = 'none';
+        
+        // Tell the Player to reset also
+        player.reset();
+    }
+
+    /* This function is called to start game and display a 
+     * game player selection overlay
+     */
+    function startGame() {
+        gameOver = false;
+        document.getElementById('game-over').style.display = 'block';
+        document.getElementById('game-over-overlay').style.display = 'block';
+    }
+
+    /* This function is called to end the current game and display a 
+     * game over overlay
+     */
+    function endGame() {
+        gameOver = false;
+        document.getElementById('game-over').style.display = 'block';
+        document.getElementById('game-over-overlay').style.display = 'block';
     }
 
     /* Go ahead and load all of the images we know we're going to need to
@@ -172,7 +218,17 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/char-boy.png',
+        'images/char-cat-girl.png',
+        'images/char-horn-girl.png',
+        'images/char-pink-girl.png',
+        'images/char-princess-girl.png',
+        'images/Gem Blue.png',
+        'images/Gem Green.png',
+        'images/Gem Orange.png',
+        'images/Heart.png',
+        'images/Key.png',
+        'images/Star.png'
     ]);
     Resources.onReady(init);
 
@@ -181,4 +237,9 @@ var Engine = (function(global) {
      * from within their app.js files.
      */
     global.ctx = ctx;
+
+    return {
+        startGame: startGame,
+        endGame: endGame
+    }
 })(this);
